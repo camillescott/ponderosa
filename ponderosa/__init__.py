83c6dd90d120da53c220cf8f4d2d03118519fb25
Camille Scott <camille.scott.w@gmail.com>
2024-11-03 07:45:54 -0800

    Bump version: 0.4.0

--- ponderosa/__init__.py
+++ ponderosa/__init__.py
@@ -6,7 +6,7 @@ from .ponderosa import (ArgAdderFunc,
                      SubCmd,
                      arggroup)
 
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 __all__ = ['ArgAdderFunc',
            'ArgGroup',
            'ArgParser',
